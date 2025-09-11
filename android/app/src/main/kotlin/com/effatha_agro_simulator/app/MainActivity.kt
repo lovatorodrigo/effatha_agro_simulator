66d624c814025b2c64793f9d900f726af397458b
lovatorodrigo <lovato.rodrigo@gmail.com>
2025-09-11 11:02:11 -0300

    Update MainActivity.kt

--- android/app/src/main/kotlin/com/effatha_agro_simulator/app/MainActivity.kt
+++ android/app/src/main/kotlin/com/effatha_agro_simulator/app/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.effatha_agro_simulator.app
 
-import io.flutter.embedding.android.FlutterFragmentActivity
-import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterFragmentActivity() {
-}
+class MainActivity : FlutterActivity()
