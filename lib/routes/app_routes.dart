3090b36d6ed45f0a486df25c631a00b7022c53c4
lovatorodrigo <lovato.rodrigo@gmail.com>
2025-09-19 14:16:35 -0300

    Update app_routes.dart

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,29 +1,20 @@
 // lib/routes/app_routes.dart
 import 'package:flutter/widgets.dart';
 
-import '../presentation/login_screen/login_screen.dart';
-import '../presentation/registration_screen/registration_screen.dart';
-import '../presentation/password_reset_screen/password_reset_screen.dart';
 import '../presentation/simulation_dashboard/simulation_dashboard.dart';
 import '../presentation/settings_screen/settings_screen.dart';
 import '../presentation/export_results_screen/export_results_screen.dart';
 
 class AppRoutes {
-  static const String initial = '/';
-  static const String passwordReset = '/password-reset-screen';
+  static const String initial = '/simulation-dashboard';
   static const String settings = '/settings-screen';
   static const String exportResults = '/export-results-screen';
-  static const String login = '/login-screen';
   static const String simulationDashboard = '/simulation-dashboard';
-  static const String registration = '/registration-screen';
 
   static Map<String, WidgetBuilder> routes = {
-    initial: (context) => LoginScreen(),
-    passwordReset: (context) => PasswordResetScreen(),
+    initial: (context) => SimulationDashboard(),
     settings: (context) => SettingsScreen(),
     exportResults: (context) => ExportResultsScreen(),
-    login: (context) => LoginScreen(),
     simulationDashboard: (context) => SimulationDashboard(),
-    registration: (context) => RegistrationScreen(),
   };
 }
